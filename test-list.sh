c1a24d2d4cd1a66200b083e8718d59eabb44b639
Salts <matthew.galligan@gmail.com>
2026-09-08 14:08:38 -0400

    Update test-list.sh

--- test-list.sh
+++ test-list.sh
@@ -1,6 +1,4 @@
 #I have no idea if this even works, the idea here is to check if there is a secret, before actually running it
 tools vault help vault.list vault.fill
-tools vault list --kind login | jq '[.[] | select(.name | ascii_downcase | test("discord"))]'
-
 
 #if no, then alert the user, somehow??
