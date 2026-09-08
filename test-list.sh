5f118f5805a71f5ecae90cfc7a77987dd5f08e56
Salts <matthew.galligan@gmail.com>
2026-09-08 14:10:18 -0400

    Update test-list.sh

--- test-list.sh
+++ test-list.sh
@@ -1,4 +1,4 @@
 #I have no idea if this even works, the idea here is to check if there is a secret, before actually running it
-tools vault help vault.list vault.fill
+tools help vault.list vault.fill
 
 #if no, then alert the user, somehow??
